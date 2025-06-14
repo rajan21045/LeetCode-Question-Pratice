46a05b98bd2d05363d9c82a3f686bead8589c3ca
Rajan Poudel <raajan.works@gmail.com>
2025-06-14 15:48:36 +0545

    Add files via upload

---
+++ Using Java/mathSqrtExample.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class mathSqrtExample {
+
+    public static int mySqrt(int x) {
+        return (int)Math.sqrt(x);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int x = sc.nextInt();  // Fixed: you need to store the input in a variable
+
+        double result = mySqrt(x);  // Fixed: call static method properly
+        System.out.println("Square root of " + x + " is: " + result);
+
+        sc.close();
+    }
+}
