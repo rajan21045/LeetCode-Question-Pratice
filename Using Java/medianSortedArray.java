aa8bc652d7d4e17df472cb61e5a23dce3ff0cb87
Rajan Poudel <raajan.works@gmail.com>
2025-06-27 15:48:00 +0545

    Update medianSortedArray.java

--- Using Java/medianSortedArray.java
+++ Using Java/medianSortedArray.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
 class Solution {
+
     public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
         
         int merged[] = new int[nums1.length+nums2.length];
