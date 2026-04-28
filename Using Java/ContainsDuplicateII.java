04fe967cac094c10520b775941c391f040d8d3af
Rajan Poudel <raajan.works@gmail.com>
2026-04-28 09:02:06 +0545

    Add files via upload

---
+++ Using Java/ContainsDuplicateII.java
@@ -0,0 +1,44 @@
+/*
+219. Contains Duplicate II
+Difficulty: Easy
+Topics: Array, Hash Table, Sliding Window
+Link: https://leetcode.com/problems/contains-duplicate-ii/
+
+Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
+
+Example 1:
+
+Input: nums = [1,2,3,1], k = 3
+Output: true
+Example 2:
+
+Input: nums = [1,0,1,1], k = 1
+Output: true
+Example 3:
+
+Input: nums = [1,2,3,1,2,3], k = 2
+Output: false
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+-109 <= nums[i] <= 109
+0 <= k <= 105
+*/
+import java.util.HashMap;
+public class ContainsDuplicateII {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer, Integer>map = new HashMap<>();
+        for(int i=0; i<nums.length; i++){
+            if (map.containsKey(nums[i])) {
+                int num = map.get(nums[i]);
+                if(i - num <= k){
+                    return true;
+                }
+            }
+            map.put(nums[i], i);
+        }
+        return false;
+    }
+}
